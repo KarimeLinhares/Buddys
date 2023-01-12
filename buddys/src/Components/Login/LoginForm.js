18ace7bdb7ac48aa1d7493bf1bb05209f904af5d
KarimeLinhares <karime_muniz@hotmail.com>
2023-01-12 14:38:14 -0300

    add new file only for Api functions. improved validations. add effect to push token in the local storage and to get it

--- buddys/src/Components/Login/LoginForm.js
+++ buddys/src/Components/Login/LoginForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { TOKEN_POST, USER_GET } from '../../api';
 import useForm from '../../Hooks/useForm';
 import Buttons from '../Forms/Buttons';
 import Inputs from '../Forms/Inputs';
@@ -8,24 +9,36 @@ const LoginForm = () => {
 	const username = useForm();
 	const password = useForm();
 
-	function handleSubmit(event) {
+	//puxa apenas uma vez o token que já esteja salvo no local storage
+	//serve para pegar um usuário que ja esteja logado
+	React.useEffect(() => {
+		const token = window.localStorage.getItem('token');
+		if (token) {
+			getUser(token);
+		}
+	}, []);
+
+	// função que puxa o usuário após o token ir para o local storage
+	async function getUser(token) {
+		const { url, options } = USER_GET(token);
+		const response = await fetch(url, options);
+		const json = await response.json();
+		console.log(json);
+	}
+
+	async function handleSubmit(event) {
 		event.preventDefault();
 
 		if (username.validate() && password.validate()) {
-			fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify(),
-			})
-				.then((response) => {
-					console.log(response);
-					return response.json();
-				})
-				.then((json) => {
-					console.log(json);
-				});
+			const { url, options } = TOKEN_POST({
+				username: username.value,
+				password: password.value,
+			});
+
+			const response = await fetch(url, options);
+			const json = await response.json();
+			window.localStorage.setItem('token', json.token);
+			getUser(json.token);
 		}
 	}
 

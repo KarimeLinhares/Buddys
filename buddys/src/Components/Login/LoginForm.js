092282a05cefe6fcdc40cbf8b2d327be54389af7
KarimeLinhares <karime_muniz@hotmail.com>
2023-01-12 14:08:09 -0300

    add custom hook for validate email and password

--- buddys/src/Components/Login/LoginForm.js
+++ buddys/src/Components/Login/LoginForm.js
@@ -1,36 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useForm from '../../Hooks/useForm';
 import Buttons from '../Forms/Buttons';
 import Inputs from '../Forms/Inputs';
 
 const LoginForm = () => {
-	const [username, setUsername] = React.useState('');
-	const [password, setPassword] = React.useState('');
+	const username = useForm();
+	const password = useForm();
 
 	function handleSubmit(event) {
 		event.preventDefault();
-		fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({ username, password }),
-		})
-			.then((response) => {
-				console.log(response);
-				return response.json();
+
+		if (username.validate() && password.validate()) {
+			fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(),
 			})
-			.then((json) => {
-				console.log(json);
-			});
+				.then((response) => {
+					console.log(response);
+					return response.json();
+				})
+				.then((json) => {
+					console.log(json);
+				});
+		}
 	}
 
 	return (
 		<section>
 			<h1>Login</h1>
 			<form action='' onSubmit={handleSubmit}>
-				<Inputs label='User Name' type='text' name='username' />
-				<Inputs label='Password' type='password' name='password' />
+				<Inputs label='User Name' type='text' name='username' {...username} />
+				<Inputs
+					label='Password'
+					type='password'
+					name='password'
+					{...password}
+				/>
 				<Buttons>Log In</Buttons>
 			</form>
 			<Link to='/login/signUp'>Sign Up</Link>
---
+++ buddys/src/Hooks/useForm.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const types = {
+	email: {
+		regex:
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+		message: 'Invalid data',
+	},
+};
+
+const useForm = (type) => {
+	const [value, setValue] = React.useState('');
+	const [error, setError] = React.useState('');
+
+	function validate(value) {
+		if (type === false) return true;
+		if (value.length === 0) {
+			setError('Cannot be empty');
+			return false;
+		} else if (types[type] && !types[type].regex.test(value)) {
+			setError(types[type].message);
+			return false;
+		} else {
+			setError(null);
+			return true;
+		}
+	}
+
+	function onChange({ target }) {
+		if (error) validate(target.value);
+		setValue(target.value);
+	}
+
+	return {
+		value,
+		setValue,
+		onChange,
+		error,
+		validate: () => validate(value),
+		onBlur: () => validate(value),
+	};
+};
+
+export default useForm;

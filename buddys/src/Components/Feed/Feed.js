f96ad2ea1b16c174b1aa87693bad5e88e794a0d0
KarimeLinhares <karime_muniz@hotmail.com>
2023-01-27 14:57:32 -0300

    fix bug in my account, show only my pics

--- buddys/src/Components/Feed/Feed.js
+++ buddys/src/Components/Feed/Feed.js
@@ -2,14 +2,14 @@ import React from 'react';
 import FeedModal from './FeedModal';
 import FeedPhotos from './FeedPhotos';
 
-const Feed = () => {
+const Feed = ({ user }) => {
 	const [modalPhoto, setModalPhoto] = React.useState(null);
 	return (
 		<div>
 			{modalPhoto && (
 				<FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />
 			)}
-			<FeedPhotos setModalPhoto={setModalPhoto} />
+			<FeedPhotos user={user} setModalPhoto={setModalPhoto} />
 		</div>
 	);
 };
--- buddys/src/Components/Feed/FeedPhotos.js
+++ buddys/src/Components/Feed/FeedPhotos.js
@@ -6,17 +6,17 @@ import { PHOTOS_GET } from '../../Api';
 import Loading from '../Helper/Loading';
 import styles from './FeedPhotos.module.css';
 
-const FeedPhotos = ({ setModalPhoto }) => {
+const FeedPhotos = ({ user, setModalPhoto }) => {
 	const { data, loading, error, request } = useFetch();
 
 	React.useEffect(() => {
 		async function fetchPhotos() {
-			const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+			const { url, options } = PHOTOS_GET({ page: 1, total: 6, user });
 			const { json } = await request(url, options);
 			console.log(json);
 		}
 		fetchPhotos();
-	}, [request]);
+	}, [request, user]);
 
 	if (error) return <Error error={error} />;
 	if (loading) return <Loading />;

75de807a1e84161b45d734773211cf2cc826f3ae
KarimeLinhares <karime_muniz@hotmail.com>
2023-01-30 13:17:09 -0300

    add page for lost password

--- buddys/src/Components/Login/LoginLost.js
+++ buddys/src/Components/Login/LoginLost.js
@@ -1,7 +1,52 @@
 import React from 'react';
+import Inputs from '../Forms/Inputs';
+import Buttons from '../Forms/Buttons';
+import useForm from '../../Hooks/useForm';
+import useFetch from '../../Hooks/useFetch';
+import { PASSWORD_LOST } from '../../Api';
+import Error from '../Helper/Error';
 
 const LoginLost = () => {
-	return <div>LoginLost</div>;
+	const login = useForm();
+	const { data, loading, error, request } = useFetch();
+
+	console.log(window.location);
+	async function handleSubmit(event) {
+		event.preventDefault();
+		if (login.validate()) {
+			const { url, options } = PASSWORD_LOST({
+				login: login.value,
+				url: window.location.href.replace('passwordLost', 'passwordReset'),
+			});
+			const { json } = await request(url, options);
+			console.log(json);
+		}
+	}
+
+	return (
+		<section>
+			<h1 className='title'>Lost Your Password?</h1>
+			{data ? (
+				<p style={{ color: '#4c1' }}>{data}</p>
+			) : (
+				<form onSubmit={handleSubmit}>
+					<Inputs
+						label='User Name / Email '
+						type='text'
+						name='login'
+						{...login}
+					/>
+					{loading ? (
+						<Buttons disabled>Sending...</Buttons>
+					) : (
+						<Buttons>Send Email</Buttons>
+					)}
+				</form>
+			)}
+
+			<Error error={error} />
+		</section>
+	);
 };
 
 export default LoginLost;

7a786c8da35a1614f6c4ac38d87ff285e8d38eb3
KarimeLinhares <karime_muniz@hotmail.com>
2023-01-30 13:25:40 -0300

    add page to reset password

--- buddys/src/Components/Login/LoginReset.js
+++ buddys/src/Components/Login/LoginReset.js
@@ -1,7 +1,59 @@
 import React from 'react';
+import Inputs from '../Forms/Inputs';
+import Buttons from '../Forms/Buttons';
+import useForm from '../../Hooks/useForm';
+import useFetch from '../../Hooks/useFetch';
+import { PASSWORD_RESET } from '../../Api';
+import Error from '../Helper/Error';
+import { useNavigate } from 'react-router-dom';
 
 const LoginReset = () => {
-	return <div>LoginReset</div>;
+	const [login, setLogin] = React.useState('');
+	const [key, setKey] = React.useState('');
+	const password = useForm();
+	const { error, loading, request } = useFetch();
+	const navigate = useNavigate();
+
+	React.useEffect(() => {
+		const params = new URLSearchParams(window.location.search);
+		const key = params.get('key');
+		const login = params.get('login');
+		if (key) setKey(key);
+		if (login) setLogin(login);
+	}, []);
+
+	async function handleSubmit(event) {
+		event.preventDefault();
+		if (password.validate()) {
+			const { url, options } = PASSWORD_RESET({
+				login,
+				key,
+				password: password.value,
+			});
+			const { response } = await request(url, options);
+			if (response.ok) navigate('/login');
+		}
+	}
+
+	return (
+		<div>
+			<h1 className='title'>Redefine your Password</h1>
+			<form onSubmit={handleSubmit}>
+				<Inputs
+					label='New Password'
+					type='password'
+					name='password'
+					{...password}
+				/>
+				{loading ? (
+					<Buttons disabled>Redefining...</Buttons>
+				) : (
+					<Buttons>Redefine</Buttons>
+				)}
+			</form>
+			<Error error={error} />
+		</div>
+	);
 };
 
 export default LoginReset;
